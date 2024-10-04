05dfc7612dafda9cb3b87b17b586600a6e323f68
Andrey Novikov <novikov@gmail.com>
2024-10-05 02:38:54 +0300

    Track state

--- components/album/index.js
+++ components/album/index.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { MasonryPhotoAlbum } from 'react-photo-album'
 import Lightbox from 'yet-another-react-lightbox'
@@ -17,6 +17,26 @@ export default function Album(props) {
 
     const [index, setIndex] = useState(-1)
 
+    const handleBackEvent = (event) => {
+        if (event.state)
+            setIndex(-1)
+    }
+
+    useEffect(() => {
+        if (window.location.hash.startsWith('#view-')) {
+            const hs = window.location.hash.split('-')
+            setIndex(hs[1])
+            const scrollTo = hs[2]
+            setTimeout(() => { // for some reason album is not ready on first render
+                const thumbnail = document.getElementById('thumbnail-' + scrollTo)
+                if (thumbnail)
+                    thumbnail.scrollIntoView({behavior: 'instant', block: 'center'})
+            }, 100)
+        }
+        window.addEventListener('popstate', handleBackEvent)
+        return () => window.removeEventListener('popstate', handleBackEvent)
+    }, [])
+
     return <>
         <MasonryPhotoAlbum
             photos={photos}
@@ -25,7 +45,7 @@ export default function Album(props) {
                 if (containerWidth < 400) return 1
                 if (containerWidth < 800) return 2
                 if (containerWidth < 1200) return 3
-                return 4
+                return 5
             }}
             spacing={(containerWidth) => {
                 if (containerWidth < 400) return 5
@@ -33,16 +53,44 @@ export default function Album(props) {
                 if (containerWidth < 1200) return 10
                 return 15
             }}
-            onClick={({ index }) => setIndex(index)} />
+            onClick={({ index }) => setIndex(index)}
+            render={{
+                image: (props, { photo }) => <img {...props} id={`thumbnail-${photo.id}`} />
+            }} />
         <Lightbox
             slides={photos}
             open={index >= 0}
             index={index}
-            close={() => setIndex(-1)}
+            close={() => {
+                setIndex(-1)
+                const location = window.location.href.split('#')[0]
+                window.history.replaceState({}, '', location)
+            }}
+            on={{
+                view: ({ index: currentIndex }) => {
+                    setIndex(currentIndex)
+                    const location = window.location.href.split('#')[0] + '#view-' + currentIndex + '-' + photos[currentIndex].id
+                    if (window.location.hash.startsWith('#view-'))
+                        window.history.replaceState({ view: photos[currentIndex].id }, '', location)
+                    else
+                        window.history.pushState({ view: photos[currentIndex].id }, '', location)
+                }
+            }}
             carousel={{
                 preload: 0
             }}
             plugins={[Download, Fullscreen, Zoom]}
+            render={{
+                buttonPrev: photos.length <= 1 ? () => null : undefined,
+                buttonNext: photos.length <= 1 ? () => null : undefined,
+                /*
+                slideFooter: () => (
+                    <div style={{ position: "absolute", color: "white", bottom: 16, left: 16 }}>
+                        Custom slide element
+                    </div>
+                )
+                */
+            }}
         />
     </>
 }
