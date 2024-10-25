cf29ed38996474a5052c1439bdd88965340cbfe2
Andrey Novikov <novikov@gmail.com>
2024-10-25 11:23:35 +0000

    History should be always dynamic

--- app/history/page.js
+++ app/history/page.js
@@ -2,6 +2,9 @@ import Link from 'next/link'
 
 import { getLogUsers } from '@/lib/db'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default async function History() {
     const users = await getLogUsers()
 
