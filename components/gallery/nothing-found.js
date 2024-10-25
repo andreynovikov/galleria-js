c991ef1bdfce09d237460d4a4720bdaa98dbb1d2
Andrey Novikov <novikov@gmail.com>
2024-10-25 21:06:20 +0300

    Adjust styling

--- components/gallery/nothing-found.js
+++ components/gallery/nothing-found.js
@@ -2,10 +2,10 @@ import Image from 'next/image'
 
 import emptyBox from '@/assets/empty-box.svg'
 
-import styles from './nothing-found.module.css'
+import styles from './nothing-found.module.scss'
 
 export default function NothingFound() {
     return <div className={styles.container}>
-        <Image src={emptyBox} alt="Nothing found" unoptimized className={styles.image} />
+        <Image src={emptyBox} alt="Nothing found" unoptimized className="image" />
     </div>
 }
