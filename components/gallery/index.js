981310ba842722ef29144c4126535da65d9155d3
Andrey Novikov <novikov@gmail.com>
2024-12-28 10:04:00 +0000

    Fix download

--- components/gallery/index.js
+++ components/gallery/index.js
@@ -69,7 +69,7 @@ export default async function Gallery({ bundle, searchParams }) {
         {
             id: image.id,
             src: `${basePath}${image.bundle}/${image.name}`,
-            download: image.restricted && user.id === undefined ? false : `${basePath}${image.bundle}/${image.name}?format=original`,
+            download: image.restricted && session?.user?.id === undefined ? false : `${basePath}${image.bundle}/${image.name}?format=original`,
             title: image.name,
             width: image.width,
             height: image.height,
